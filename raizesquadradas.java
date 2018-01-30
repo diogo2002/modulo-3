870326710ed3329d2844a6571bc2e9384349ebe0
diogo2002 <32169278+diogo2002@users.noreply.github.com>
2018-01-30 10:52:32 +0000

    Add files via upload

---
+++ raizesquadradas.java
@@ -0,0 +1,16 @@
+
+
+package raizesquadradas;
+
+public class raizesquadradas {
+ static void Raizesquadradas(int Inf, int sup)
+ {for(int I=Inf; I<=sup;I++)
+  System.out.print(String.format("%5.1f", Math.sqrt(I)));       }
+    
+    public static void main(String[] args) 
+    {Raizesquadradas(25,36);
+    Raizesquadradas(100,120);
+        // TODO code application logic here
+    }
+    
+}
