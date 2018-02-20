de842a6c25a055e5253d13f43dbfeb80459ec7ff
diogo2002 <32169278+diogo2002@users.noreply.github.com>
2018-02-20 11:26:12 +0000

    Add files via upload

---
+++ medias.java
@@ -0,0 +1,25 @@
+package oi;
+import java.util.Scanner;
+public class medias {
+
+	public static void main(String[] args) {
+		{int N;
+		double Med = 0;
+	    Scanner LerS = new Scanner (System.in);
+	    System.out.printf("Quantos elementos tem o vetor?");
+	    N = LerS.nextInt();
+	    double[]A=new double [N];
+	    for(int I=0; I<=N-1; I++)
+	    {System.out.printf("Elemento %d =", I+1);
+	    A[I]=LerS.nextDouble();}
+	    for(int I=0;I<=N-1;I++)
+	    Med+=A[I];
+	    Med=Med/N;
+	    System.out.printf(String.format("Media aritmetica = %5.2f", Med));
+	    
+	    }
+		}
+
+	}
+
+
